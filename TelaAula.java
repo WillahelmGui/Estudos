ad39d985e1aa7066d605f9e3838f748fda569855
WillahelmGui <108191887+WillahelmGui@users.noreply.github.com>
2022-09-24 12:59:27 -0300

    Add files via upload

--- TelaAula.java
+++ TelaAula.java
@@ -8,38 +8,39 @@ public class TelaAula {
 	public static void main(String[] args) {
 
 		Scanner sc1 = new Scanner(System.in);
-
-		System.out.println("************************* \n Bem Vindo \n*************************");
-		System.out.println(" 1- Chamada \n 2- Cadastro \n 3- Relatório \n 0- Sair \n*************************");
-		String kp = sc1.next();
-
+		int kp;
 		do {
+			System.out.println("************************* \n Bem Vindo \n*************************");
+			System.out.println(" 1- Chamada \n 2- Cadastro \n 3- Relatório \n 0- Sair \n*************************");
+			kp = sc1.nextInt();
+
 			switch (kp) {
-			case "1":
+			case 1:
 				System.out.println("Carregando...");
 				break;
 
-			case "2":
+			case 2:
 				System.out.println("Digite o nome do aluno para iniciar o cadastro");
+				sc1.nextLine();
 				String nome = sc1.nextLine();
 				System.out.println("Digite a data de nascimento para concluir o cadastro");
 				String nascimento = sc1.next();
 				System.out.println("Dados do aluno cadastrado:" + nome + " " + nascimento);
 				break;
 
-			case "3":
+			case 3:
 				System.out.println("Relatório não disponível.");
 				break;
 
-			case "0":
+			case 0:
 				System.exit(1);
 				break;
 
 			default:
 				System.out.println("Digite um número válido");
 			}
-		} while (kp != "0");
+
+		} while (kp != 0);
 		sc1.close();
 	}
-
 }
